c5b0b50775e3e962726e605f1b4e80d664c48b70
Ido Strauss <strauss02@hotmail.co.il>
2022-01-10 00:38:52 +0200

    establish connection to local DB

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql2')
 const PORT = 3001
+const teacherRouter = require('./teacher')
 require('dotenv').config()
-
 const dbConfig = {
   host: process.env.MYSQL_HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 }
-
 const db = mysql.createConnection(dbConfig)
+db.connect((err) => {
+  if (err) console.log(err)
+  console.log('connected to MYSQL DB')
+})
 
-const teacherRouter = require('./teacher')
+app.use('/teacher', teacherRouter)
 
 app.get('/', (req, res) => {
   res.send('welcome')
 })
-
-app.use('/teacher', teacherRouter)
 
 app.listen(PORT, console.log(`Listening on ${PORT}`))
