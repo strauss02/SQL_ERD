d9ddad7dcf4e033adf457221e7dd2d5c0248f329
Ido Strauss <strauss02@hotmail.co.il>
2022-01-10 00:28:34 +0200

    add db connection

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
 const PORT = 3001
+require('dotenv').config()
+
+const dbConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+}
+
+const db = mysql.createConnection(dbConfig)
 
 const teacherRouter = require('./teacher')
 
