3a5d17b88c4d6f5bbf28c0e8cb3b062248383821
Ido Strauss <strauss02@hotmail.co.il>
2022-01-10 19:53:29 +0200

    add teacher insertion path

--- teacher.js
+++ teacher.js
@@ -14,4 +14,17 @@ router.get('/:id', (req, res) => {
   })
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+
+  const { id, full_name, teaches_subjects } = req.body
+
+  const sql = `
+  INSERT INTO mydb.teachers (id, full_name, teaches_subjects) VALUES (${id}, '${full_name}', '${teaches_subjects}')`
+  db.query(sql, function (err, result) {
+    if (err) throw err
+    res.send(JSON.stringify(result))
+  })
+})
+
 module.exports = router
