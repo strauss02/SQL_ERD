c15ff04c8796cc7497be9e992de3d38652765139
Ido Strauss <strauss02@hotmail.co.il>
2022-01-10 15:49:23 +0200

    seperate connection to its own module

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express()
 const mysql = require('mysql2')
 const PORT = 3001
 const teacherRouter = require('./teacher')
-const db = require('./db')
+require('dotenv').config()
+const dbConfig = {
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+}
+const db = mysql.createConnection(dbConfig)
+db.connect((err) => {
+  if (err) console.log(err)
+  console.log('connected to MYSQL DB')
+})
 
 app.get('/test', (req, res) => {
   const sql = `SELECT * FROM students`
--- teacher.js
+++ teacher.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mysql = require('mysql2')
 
 const router = express.Router()
 
